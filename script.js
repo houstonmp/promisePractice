20d30cfeab0b3f053233479aaf094c150bb1a4c1
Matthew Houston <matthouston027@gmail.com>
2023-02-15 14:30:34 +0900

    Add styles and check for errors with then and catch

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const changeSectionStyle = (el) => {
             }
             else {
                 resolve(`Style updated for ${el}`);
-                // document.querySelector(`#${el}`).classList.add(`${el}`);
+                document.querySelector(`#${el}`).classList.add(`${el}`);
             }
         }, delay);
     })
@@ -18,19 +18,13 @@ changeSectionStyle('sect1')
         console.log('Style 1 Changed!');
         return changeSectionStyle('sect2');
     })
-    .catch(() => {
-        console.log(`Error: Style 1 not changed`)
-    })
     .then(() => {
         console.log('Style 2 Changed!');
         return changeSectionStyle('sect3');
-    })
-    .catch(() => {
-        console.log(`Error: Style 2 not changed`)
     })
     .then(() => {
         console.log('Style 3 Changed!');
     })
     .catch(() => {
-        console.log(`Error: Style 3 not changed`)
+        console.log("Error: A style was not changed")
     })
